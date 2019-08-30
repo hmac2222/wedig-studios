cdc6b50b41268bfcd53624ed5b74e7003a7f08ad
hmac <henry.macafee@gmail.com>
2019-08-30 15:59:34 -0500

    More css tweaks (contact form responsiveness)

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,9 @@ const ContactPage = () => (
     <Layout>
         <SEO title="Page two" />
         <h2 className="make-music">We'd love to help you with your project!</h2>
-        <h3>Tell us a little bit about what you're working on:</h3>
+        <div className="book-button-container"><a className="book-now" href="#">CALL US: 555-555-5555</a></div>
+
+        <h3>...or tell us a little bit about what you're working on:</h3>
         <div className="contact-container"><form className="contact-form" method="post" action="#">
             <label>
                 Name
